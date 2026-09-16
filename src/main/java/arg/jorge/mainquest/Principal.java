eed7938da233d0a2a556925a4ed0814ec21d0a20
JorgeLOG123 <jorgeluisordonezguevara@gmail.com>
2026-09-16 20:03:41 -0300

    Aplicando buenas practivas, Fragment, Thymeleaf anti Hardcodeo.

--- src/main/java/arg/jorge/mainquest/Principal.java
+++ src/main/java/arg/jorge/mainquest/Principal.java
@@ -1,12 +1,19 @@
 package arg.jorge.mainquest;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Principal {
 
     public static void main(String[] args) {
         SpringApplication.run(Principal.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 }
